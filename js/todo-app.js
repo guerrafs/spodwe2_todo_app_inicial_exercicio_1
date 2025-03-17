dbb78502a33604cecf776b2cf4d2913409b03e28
Fabricio Guerra <103272551+guerrafs@users.noreply.github.com>
2025-03-17 18:45:29 +0000

    copia versão do repositorio da aula

--- js/todo-app.js
+++ js/todo-app.js
@@ -1,62 +1,57 @@
 const todos = [];
 
-const renderTodos = function () {
-    const todoListUl = document.getElementById("todo-list");
-    
-    todoListUl.innerHTML = "";
-
-    for(const todo of todos){
-        const todoItemLi = document.createElement("li");
-        todoItemLi.textContent = todo.text;
-    
-        if(!todo.done){
-            const markTodoAsDoneButton = document.createElement("button");
-            markTodoAsDoneButton.textContent = "Marcar como concluí­do";
-        
-            markTodoAsDoneButton.onclick = function(e) {
-                markTodoAsDone(todo.id);
-                renderTodos();
-            }
-        
-            todoItemLi.appendChild(markTodoAsDoneButton);
-        } else {
-            todoItemLi.style.textDecoration = "line-through";
-        }
-        todoListUl.appendChild(todoItemLi);
-    }   
-}
-
-
-document.getElementById("new-todo").addEventListener("keypress", function(e){    
-    if(e.key === "Enter"){
+document.getElementById("new-todo").addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
         const newTodoInput = document.getElementById("new-todo");
+        const todoText = newTodoInput.value.trim();
+        if (todoText === "") return;
 
-        const todoInputValue = newTodoInput.value.trim();
-
-        if(todoInputValue === "") return;
-        
-        addTodo(todoInputValue);
-        
+        addTodo(todoText);
         newTodoInput.value = "";
         renderTodos();
     }
 });
 
-function markTodoAsDone(todoId){
-    const todoToMark = todos.find((todo) => todo.id === todoId);
-    todoToMark.done = true;
+function renderTodos() {
+    const todoListUl = document.getElementById("todo-list");
+
+    todoListUl.innerHTML = "";
+
+    for (const todo of todos) {
+        const todoItemLi = document.createElement("li");
+        todoItemLi.textContent = todo.text;
+
+        if (!todo.done) {
+            const markTodoAsDoneButton = document.createElement("button");
+            markTodoAsDoneButton.textContent = "Concluir";
+            markTodoAsDoneButton.onclick = function () {
+                todo.done = true;
+                renderTodos();
+            };
+            todoItemLi.appendChild(markTodoAsDoneButton);
+        } else {
+            todoItemLi.style.textDecoration = "line-through";
+        }
+
+        todoListUl.appendChild(todoItemLi);
+    }
 }
 
-function addTodo(todoText){
-    const lastId = (todos.length > 0? todos[todos.length - 1].id : 0);
-    
+function addTodo(todoText) {
+    const lastId = todos.length > 0 ? todos[todos.length - 1].id : 0;
+
     const newTodo = {
         id: lastId + 1,
         text: todoText,
-        done: false
+        done: false,
     };
 
     todos.push(newTodo);
+}
+
+function markTodoAsDone(todoId) {
+    const todo = todos.find((todo) => todo.id === todoId);
+    todo.done = true;
 }
 
 renderTodos();
